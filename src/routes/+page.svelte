fc531ad0a8393d65e6435481ecf6749766c69237
antho <anthonyboisset06@gmail.com>
2023-09-22 11:26:49 +0200

    add btn and text to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,47 @@
 
 </script>
 
+<div class="all">
 
-<h1>Bienvenue dans le smintisme !</h1>
+    <h1>Bienvenue dans le smintisme !</h1>
+    
+    <img src="SMINT.png" alt="SMINT" width="100%"/>
+    
+    <!-- <video autoplay width="100%" loop >
+        <source src="propagande.mp4" type="video/mp4">
+            <track kind="captions">
+            </video> -->
+            
+    <button class="histoire"> <a href="/histoire">Notre histoire</a> </button>
 
-<img src="SMINT.png" alt="SMINT" width="100%"/>
-
-<!-- <video autoplay width="100%" loop >
-    <source src="propagande.mp4" type="video/mp4">
-    <track kind="captions">
-</video> -->
+    <p style="text-align: center;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus exercitationem eos harum, nisi recusandae impedit doloribus molestiae ea quae ducimus enim reprehenderit unde tempora omnis! Molestiae necessitatibus ipsam eos est nihil aliquam nesciunt illum distinctio eaque ad eligendi atque tempore omnis sed aut, sint perferendis. Reiciendis temporibus velit nihil.</p>
+</div>
 
 <style>
+
+    .all {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 50px;
+        padding: 10px;
+    }
+
     h1 {
         margin-top: 10px;
         text-align: center;
+        font-size: 3rem;
     }
     video {
         object-fit: contain;
         width: "100%";
+    }
+
+    button.histoire a {
+        padding: 10px;
+        background-color: var(--dark-blue);
+        color: white;
+        text-transform: uppercase;
+        cursor: pointer;
     }
 </style>
