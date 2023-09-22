9282931e5631e1ce2080172c7f52e6ed556c1649
antho <anthonyboisset06@gmail.com>
2023-09-22 08:53:57 +0200

    basic menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
     import "../app.css"
+
+    import Menu from "svelte-material-icons/Menu.svelte";
 </script>
 
+<header>
+    <img src="smint_logo.png" alt="smint logo" height="40px"/>
+    <Menu color="white" height="40px" width="35px"/> 
+</header>
+
 <slot />
+
+<style>
+    header {
+        background-color: var(--dark-blue);
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+    }
+</style>
