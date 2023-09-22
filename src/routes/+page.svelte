6ccbddf5f5ce0f256b1b177f39d24a7795a3a30d
antho <anthonyboisset06@gmail.com>
2023-09-22 10:55:44 +0200

    add video to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Bienvenue dans le smintisme</h1>
+<h1>Bienvenue dans le smintisme !</h1>
 
+<video controls autoplay  width="100%">
+    <source src="propagande.mp4" type="video/mp4">
+    <track kind="captions">
+</video>
+
+<style>
+    h1 {
+        margin-top: 10px;
+        text-align: center;
+    }
+    video {
+        object-fit: contain;
+        width: "100%";
+    }
+</style>
